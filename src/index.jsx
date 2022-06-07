be54343ec7199c60ce848885b506d8250a090632
Nilay Sondagar <nilaysondagar@Nilays-MBP.lan>
2022-06-06 20:44:13 -0700

    Initial Setup - Added Prettier and ESLint - Minor configuration changes - Enabled SCSS processing for styles - Assorted formatting fixes

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import './index.scss'
+
+const root = document.getElementById('root')
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    root
+)
